75ac69f707ea3675927a8ddd1e9e7971f12b3e4b
Rudransh Arora <72294547+Red-0111@users.noreply.github.com>
2023-09-09 09:13:29 +0530

    Time: 0 ms (100%), Space: 6.5 MB (85.45%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+
+    int combinationSum4(vector<int>& nums, int tar) {
+        vector<unsigned int> dp(tar+1,0);
+        dp[0]=1;
+        for(int i=1;i<=tar;i++)
+        {
+            for(int j=0;j<nums.size();j++)
+            {
+                if(i-nums[j]>=0)
+                {
+                    dp[i]+=dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[tar];
+    }
+};
